b8b24b046b3233c68ff4bb810430e9a9bf3f5d99
IgorLoskJava <igororder@gmail.com>
2026-03-10 00:24:35 +0300

    Initial commit

---
+++ src/main/java/dev/loskutnikov/springbootmvcfinal/SpringBootMvcFinalApplication.java
@@ -0,0 +1,13 @@
+package dev.loskutnikov.springbootmvcfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/loskutnikov/springbootmvcfinal/SpringBootMvcFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.loskutnikov.springbootmvcfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
